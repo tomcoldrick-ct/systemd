367a1333d539e09d9d60a2a9bcca7e17d00ea267
Tom Coldrick <thomas.coldrick@codethink.co.uk>
2024-08-21 09:17:16 +0100

    fixup! sysupdate: Implement systemd-sysupdated dbus service

--- src/sysupdate/sysupdated.c
+++ src/sysupdate/sysupdated.c
@@ -857,7 +857,7 @@ static int target_method_list(sd_bus_message *msg, void *userdata, sd_bus_error 
         const char *details[] = {
                 "class", target_class_to_string(t->class),
                 "name", t->name,
-                "offline", one_zero(FLAGS_SET(flags, SYSUPDATE_OFFLINE)),
+                "offline", one_zero(FLAGS_SET(flags, SD_SYSTEMD_SYSUPDATE_OFFLINE)),
                 NULL
         };
 
@@ -876,7 +876,7 @@ static int target_method_list(sd_bus_message *msg, void *userdata, sd_bus_error 
         if (r < 0)
                 return r;
 
-        j->offline = FLAGS_SET(flags, SYSUPDATE_OFFLINE);
+        j->offline = FLAGS_SET(flags, SD_SYSTEMD_SYSUPDATE_OFFLINE);
 
         r = job_start(j);
         if (r < 0)
@@ -923,7 +923,7 @@ static int target_method_describe(sd_bus_message *msg, void *userdata, sd_bus_er
                 "class", target_class_to_string(t->class),
                 "name", t->name,
                 "version", version,
-                "offline", one_zero(FLAGS_SET(flags, SYSUPDATE_OFFLINE)),
+                "offline", one_zero(FLAGS_SET(flags, SD_SYSTEMD_SYSUPDATE_OFFLINE)),
                 NULL
         };
 
@@ -946,7 +946,7 @@ static int target_method_describe(sd_bus_message *msg, void *userdata, sd_bus_er
         if (!j->version)
                 return log_oom();
 
-        j->offline = FLAGS_SET(flags, SYSUPDATE_OFFLINE);
+        j->offline = FLAGS_SET(flags, SD_SYSTEMD_SYSUPDATE_OFFLINE);
 
         r = job_start(j);
         if (r < 0)
